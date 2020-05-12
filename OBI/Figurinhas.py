ca3a8ffd40947ddd5c4465e78b9b0d65185f4fc0
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2020-05-12 10:05:02 -0300

    OBI OK

--- OBI/Figurinhas.py
+++
@@ -1,9 +0,0 @@
-# 100 pontos
-n, c, m = map(int, raw_input().split())
-fig_car = map(int, raw_input().split())
-fig_comp = map(int, raw_input().split())
-faltam = c
-for e in fig_car:
-	if e in fig_comp:
-		faltam -= 1
-print faltam
