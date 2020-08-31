04cce35225c8136a1f950cfa862b0f568c51c8e3
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2020-08-31 11:38:28 -0300

    Q1401A

---
+++ Codeforces/Q1401A.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n,a,k;
+	scanf("%d",&n);
+	while(n--){
+		scanf("%d %d",&a,&k);
+		if (a <= k) {
+            cout << k - a << endl;
+        } else {
+			((a - k) % 2) ? cout << 1 << endl : cout << 0 << endl;
+		}
+	}
+	return 0;
+}
