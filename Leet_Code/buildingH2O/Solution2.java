1b68f4793bbfc7cd47528e1e70add9afe9972ca7
Gabriel Medeiros <50140935+medeirosgabriel@users.noreply.github.com>
2022-04-09 18:26:20 -0300

    buildingH20 solution 2

---
+++ Leet_Code/buildingH2O/Solution2.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.Semaphore;
+
+class H2O {
+  
+    Semaphore hydrogen,oxygen;
+  
+    public H2O() {
+        hydrogen = new Semaphore(2);
+        oxygen = new Semaphore(0);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+		    hydrogen.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        oxygen.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oxygen.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+		    releaseOxygen.run();
+        hydrogen.release(2);
+    }
+}
