cdb25478f1fc168496efba88cba7492f5497f85d
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2024-05-15 17:41:41 -0300

    count and say solution - faster solution

--- Leet_Code/countAndSay/SolutionII.java
+++ Leet_Code/countAndSay/SolutionII.java
@@ -1,5 +1,4 @@
 class Solution {
-  
     public String countAndSay(int n) {
         return countAndSay(n, "1");
     }
@@ -8,7 +7,7 @@ class Solution {
         if (n == 1) {
             return text;
         } else {
-            String newText = "";
+            StringBuilder sb = new StringBuilder();
             int count = 0;
             char c = text.charAt(0);
             for (int i = 0; i < text.length(); i++) {
@@ -16,17 +15,17 @@ class Solution {
                 if (cc == c) {
                     count += 1;
                 } else {
-                    newText += String.valueOf(count) + String.valueOf(c);
-                    count = 1;
-                    c = cc;
+                    sb.append(String.valueOf(count));
+                    sb.append(String.valueOf(c));
+                    count = 1; c = cc;
                 }
                 if (i == text.length() - 1) {
-                    newText += String.valueOf(count) + String.valueOf(c);
-                    count = 1;
-                    c = cc;
+                    sb.append(String.valueOf(count));
+                    sb.append(String.valueOf(c));
+                    count = 1; c = cc;
                 }
             }
-            return countAndSay(n - 1, newText);
+            return countAndSay(n - 1, sb.toString());
         }
     }
 }
