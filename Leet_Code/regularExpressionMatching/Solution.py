bc7684adff02dbb51ff2f606fbe0711abffef766
Gabriel Medeiros <50140935+medeirosgabriel@users.noreply.github.com>
2024-05-16 23:32:09 -0300

    regular expression matching

---
+++ Leet_Code/regularExpressionMatching/Solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        ls, lp = len(s), len(p)
+        dp = [[False for _ in range(lp + 1)] for _ in range(ls + 1)]
+        dp[0][0] = True
+        for i in range(ls + 1):
+            for j in range(1, lp + 1):
+                if (p[j - 1] == "*"):
+                    dp[i][j] = dp[i][j - 2] or (i > 0 and dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
+                else:
+                    dp[i][j] = i > 0 and dp[i - 1][j - 1] and (s[i - 1] == p[j - 1] or p[j - 1] == '.')
+
+        return dp[ls][lp]
