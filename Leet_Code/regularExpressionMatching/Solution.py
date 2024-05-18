a6776535697e2b8a08ffc0793005a13d4c491e29
Gabriel Medeiros <50140935+medeirosgabriel@users.noreply.github.com>
2024-05-18 18:26:03 -0300

    Update Solution.py

--- Leet_Code/regularExpressionMatching/Solution.py
+++ Leet_Code/regularExpressionMatching/Solution.py
@@ -11,6 +11,8 @@ class Solution(object):
         for i in range(ls + 1):
             for j in range(1, lp + 1):
                 if (p[j - 1] == "*"):
+                    # a a  OR a a
+                    # a *     . *
                     dp[i][j] = dp[i][j - 2] or (i > 0 and dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
                 else:
                     dp[i][j] = i > 0 and dp[i - 1][j - 1] and (s[i - 1] == p[j - 1] or p[j - 1] == '.')
