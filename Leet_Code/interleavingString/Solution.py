7aeb33869b07d2a92fd4e574ea9eb7e331fcd7ef
Gabriel Medeiros <50140935+medeirosgabriel@users.noreply.github.com>
2024-06-25 23:50:09 -0300

    Update Solution.py

--- Leet_Code/interleavingString/Solution.py
+++ Leet_Code/interleavingString/Solution.py
@@ -26,4 +26,35 @@ class Solution(object):
                 if (s2[j - 1] == s3[i + j - 1]):
                     dp[i][j] = dp[i][j] or dp[i][j - 1]
         return dp[-1][-1]
-        
+
+## BACKTRACKING
+
+class Solution(object):
+    def backTracking(self, s1, i, s2, j, s3, k):
+        if (k == len(s3)):
+            return True
+        r1, r2 = False, False
+        if (i < self.ls1 and s1[i] == s3[k]):
+            r1 = self.backTracking(
+                s1, i + 1, 
+                s2, j, 
+                s3, k + 1
+            )
+        if (j < self.ls2 and s2[j] == s3[k]):
+            r2 = self.backTracking(
+                s1, i, 
+                s2, j + 1, 
+                s3, k + 1
+            )
+        return r1 or r2
+
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        self.ls1, self.ls2, self.ls3 = len(s1), len(s2), len(s3)
+        if (self.ls1 + self.ls2 != self.ls3): return False
+        return self.backTracking(s1, 0, s2, 0, s3, 0)
