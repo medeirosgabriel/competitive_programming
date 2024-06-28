68050bf89fb7e459052df736b54a62bfdbb4e965
Gabriel Medeiros <50140935+medeirosgabriel@users.noreply.github.com>
2024-06-27 22:34:49 -0300

    Update Solution.py

--- Leet_Code/pathWithMaximumProbability/Solution.py
+++ Leet_Code/pathWithMaximumProbability/Solution.py
@@ -10,28 +10,24 @@ class Solution(object):
         :type end_node: int
         :rtype: float
         """
-        inf = float("inf")
-        adj = [[] for i in range(n)]
-        visited = [False] * n
-        probs = [-1] * n
-        probs[start_node] = 1
-
+        adj = [[] for _ in range(n)]
         for i in range(len(edges)):
-            a, b = edges[i]
-            succ = succProb[i]
-            adj[a].append((b, succ))
-            adj[b].append((a, succ))
-        
+            a,b = edges[i]
+            prob = succProb[i]
+            adj[a].append((b, prob))
+            adj[b].append((a, prob))
+
+        probs = [0] * n
+        visited = [False] * n
+        probs[start_node] = 1
         queue = PriorityQueue()
-        visited[start_node] = True
         queue.put((1, start_node))
-        while (not queue.empty()):
+        while(not queue.empty()):
             _, node = queue.get()
             visited[node] = True
-            for node_n, prob_n in adj[node]:
-                if not visited[node_n] and probs[node] * prob_n > probs[node_n]:
-                    probs[node_n] = probs[node] * prob_n
-                    queue.put((probs[node_n] * -1, node_n))
-
-        resp = probs[end_node]
-        return 0 if (resp == -1) else resp
+            for node_n, prob in adj[node]:
+                if (not visited[node_n] and probs[node] * prob > probs[node_n]):
+                    probs[node_n] = probs[node] * prob
+                    queue.put((probs[node] * prob * -1, node_n))
+                    
+        return probs[end_node]
