b8f32fac1e0bd1443216c6507efb3a74773ab11a
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2026-07-18 15:43:11 -0300

    flatten binary tree to linked list

---
+++ Leet_Code/flattenBinaryTreeToLL/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        TreeNode p = null;
+        while (curr != null) {
+            if (curr.left != null) {
+                p = curr.left;
+                while (p.right != null) {
+                    p = p.right;
+                }
+                p.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
+}
