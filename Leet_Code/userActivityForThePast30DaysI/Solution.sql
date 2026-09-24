3e907c5161a5f2988d02b93e7bcca8d823cdd69c
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2026-09-24 17:46:17 -0300

    sql question solutions

---
+++ Leet_Code/userActivityForThePast30DaysI/Solution.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select 
+    activity_date as day, 
+    count(distinct user_id) as active_users
+from activity
+where activity_date between date '2019-06-28' and date '2019-07-27'
+group by activity_date
